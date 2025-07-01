94dfacfb2664a4bc989fe01376b95dea0c8f0d6c
Satyam Singh <satyamrajput9956@gmail.com>
2025-07-01 09:47:51 +0530

    DB initialise

---
+++ Backend/src/Models/query.model.js
@@ -0,0 +1,28 @@
+import e from "express";
+import mongoose from "mongoose";
+
+const querySchema = new mongoose.Schema({
+   name:{
+    type: String,
+    required: true,
+    trim: true
+   },
+   email: {
+    type: String,
+    required: true,
+    trim: true
+   },
+    message: {
+    type: String,
+    required: true,
+    trim: true
+   },
+   phone:{
+    type: String,
+    required: true,
+    trim: true
+   }
+},{timestamps: true});
+
+const Query = mongoose.model("Query", querySchema);
+export default Query;
